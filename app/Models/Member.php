f422b6d0791196af0ac559d9c8df966102142604
pzohn <pzohn@163.com>
2019-08-03 19:39:01 +0800

    20190803-1

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -39,15 +39,8 @@ class  Member extends Model {
     }
 
     public static function memberUpdatePhone($params) {
-        \Log::info("----------", [$params]);
-        \Log::info("----------", [array_get($params,"phone")]);
-        \Log::info("----------", [array_get($params,"name")]);
-        \Log::info("----------", [array_get($params,"email")]);
-        \Log::info("----------", [array_get($params,"sex")]);
-        \Log::info("----------", [array_get($params,"age")]);
         $member = Member::where("phone", array_get($params,"phone"))->first();
         if ($member) {
-            \Log::info("9999999");
             $member->name = array_get($params,"name");
             $member->email = array_get($params,"email");
             $member->sex = array_get($params,"sex");
