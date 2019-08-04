31bfa93e2d944358d728588bdc2923168c55d494
pzohn <pzohn@163.com>
2019-08-04 18:35:32 +0800

    20190804-1

--- app/Models/Ninesquare.php
+++ app/Models/Ninesquare.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class  Ninesquare extends Model {
         
     public static function getNine() {
-        $ninesquares = \DB::select('SELECT * FROM carousels LIMIT 9');
+        $ninesquares = \DB::select('SELECT * FROM ninesquares LIMIT 9');
         if($ninesquares){
             return $ninesquares;
         }
