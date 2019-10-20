c2eedfc7bec906d95aa3f123c59c0b0f7b05a44e
pzohn <pzohn@163.com>
2019-10-20 23:00:15 +0800

    201910202259_zhangyulai
    update CollectionController.php for collection

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -10,7 +10,11 @@ class CollectionController extends Controller
     public function insertOneCollectionItem(Request $req) {
         $params_psotitem = [
             "openid" => $req->get('openid'),
-            "articalid" => $req->get('articalid')
+            "articalid" => $req->get('articalid'),
+            "title" => $req->get('title'),
+            "nickname" => $req->get('nickname'),
+            "imgurls" => $req->get('imgurls'),
+            "donetime" => $req->get('donetime'),
         ];
         $postitem = Collection::writeOneItem($params_psotitem);
         if ($postitem){
