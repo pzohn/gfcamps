9ae08ec2c4bd3031e1a81b96627246b65c41c67a
pzohn <pzohn@163.com>
2020-04-03 08:40:09 +0800

    20200403-1

--- resources/views/gfc.blade.php
+++ resources/views/gfc.blade.php
@@ -59,58 +59,8 @@
       <a href="index.html">
       <img src="images/index/logo1.png" class="rounded" alt="Cinque Terre" style="width: 20%;height: 20%;padding-top: 2%">
     </div>
-
-    <nav class="navbar navbar-expand-sm bg-light navbar-light" style="margin: 0 auto;background-color: white !important;padding-top: 20px;padding-left: 15%">
-      <ul class="navbar-nav" >
-        <li class="nav-item active">
-          <a class="nav-link" href="index.html">首页</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="nature.html">自然营</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="survive.html">生存营</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="science.html">科学营</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="humanism.html">人文营</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="camps.html">营会团</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.html">关于我们</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
+    
   <div class="body" style="padding-top: 18%">
-    <div id="demo" class="carousel slide" data-ride="carousel" style="text-align: center;">
-      <ul class="carousel-indicators">
-        <li data-target="#demo" data-slide-to="0" class="active"></li>
-        <li data-target="#demo" data-slide-to="1"></li>
-        <li data-target="#demo" data-slide-to="2"></li>
-      </ul>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="lunboimg" src="images/index/header11.jpg">
-        </div>
-        <div class="carousel-item">
-          <img class="lunboimg" src="images/index/header21.jpg">
-        </div>
-        <div class="carousel-item">
-          <img class="lunboimg" src="images/index/header31.jpg">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#demo" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#demo" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
-    </div>
     <div class="row" style="padding-left: 28%;padding-top: 50px;">
       <div style="margin-top: 25px;">
         <hr style="width: 70px;border: 1px solid gray;">
@@ -122,18 +72,6 @@
         <hr style="width: 70px;border: 1px solid gray;">
       </div>
     </div>
-
-    <div style="text-align: center;width: 100%;">
-      <video controls="controls" style="width: 100%;">
-        <source src="https://www.gfcamps.cn/video/gfc.ogg" type="video/ogg" />
-        <source src="https://www.gfcamps.cn/video/gfc.mp4" type="video/mp4" />
-        <source src="https://www.gfcamps.cn/video/gfc.webm" type="video/webm" />
-        <object data="https://www.gfcamps.cn/video/gfc.mp4"  height="240"> 
-          <embed height="240" src="https://www.gfcamps.cn/video/gfc.swf" />
-        </object>
-      </video>
-    </div>
-    <div>
 
     <div style="padding-top: 3%;">
       <div class="title" style="/*border: 1px solid #03AF7A;*/background
@@ -173,83 +111,6 @@
     </div>
 
     </div>
-    <div style="text-align: center;padding-top: 30px;">
-      <strong style="font-size: 30px;color: green;">共学体系/PARTERS</strong>
-    </div>
-
-<!-- 热点处理 -->
-<!--     <div id="demo1" class="carousel slide" data-ride="carousel" style="text-align: center;padding-top: 5%;">
-      <ul class="carousel-indicators">
-        <li data-target="#demo1" data-slide-to="0" class="active"></li>
-        <li data-target="#demo1" data-slide-to="1"></li>
-        <li data-target="#demo1" data-slide-to="2"></li>
-      </ul>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="img-thumbnail lunboimg1" usemap="#MM1" src="images/index/youmei11.jpg" width="256" height="191">
-            <map name="MM1">
-                <area shape="rect" coords="0,0,600,320" href="nature.html" alt="矩形">
-                <area shape="rect" coords="176,112,256,192" href="humanism.html" alt="矩形">
-            </map>
-        </div>
-        <div class="carousel-item">
-          <img class="img-thumbnail lunboimg1" src="images/index/youmei12.jpg">
-        </div>
-        <div class="carousel-item">
-          <img class="img-thumbnail lunboimg1" src="images/index/youmei13.jpg">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#demo1" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#demo1" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
-    </div> -->  
-
-    <div id="demo_0"></div>
-    <div id="demo_1"></div>
-    <div id="demo_2"></div>
-
-
-<!--     <div id="demo2" class="carousel slide" data-ride="carousel" style="text-align: center;padding-top: 5%;">
-      <ul class="carousel-indicators">
-        <li data-target="#demo2" data-slide-to="0" class="active"></li>
-        <li data-target="#demo2" data-slide-to="1"></li>
-        <li data-target="#demo2" data-slide-to="2"></li>
-      </ul>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <a href="camp.html?id=12">
-          <img class="img-thumbnail lunboimg1" src="images/index/yingjia11.jpg">
-        </div>
-        <div class="carousel-item">
-          <a href="camp.html?id=1">
-          <img class="img-thumbnail lunboimg1" src="images/index/yingjia12.jpg">
-        </div>
-        <div class="carousel-item">
-          <a href="camp.html?id=11">
-          <img class="img-thumbnail lunboimg1" src="images/index/yingjia13.jpg">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#demo2" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#demo2" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
-    </div> -->
-
-
-<!--     <div style="padding-top: 20px;">
-      <img src="images/index/product2.jpg" class="img-thumbnail" alt="Cinque Terre">
-    </div>
-    <div style="padding-top: 20px;">
-      <img src="images/index/product2.jpg" class="img-thumbnail" alt="Cinque Terre">
-    </div>
-    <div style="padding-top: 20px;">
-      <img src="images/index/product4.jpg" class="img-thumbnail" alt="Cinque Terre">
-    </div> -->
   </div>
   <div class="tail" style="background:url(images/index/u264.png);height: 300px;">
     <div class="row" style="padding-left: 23%;padding-top: 10px;">
@@ -270,9 +131,7 @@
       <div style="padding-left: 180px;">
         <img src="images/about/gzh20190804.jpg" class="img-thumbnail" alt="Cinque Terre" style="width: 70%;">
       </div>
-<!--       <div style="padding-left: 180px;padding-top: 20px;">
-        <img src="images/index/u26.png" class="" alt="Cinque Terre" style="width: 138px;height: 134px;">
-      </div> -->
+
       <div style="padding-left: 30%;padding-top: 30px;color: white;text-align: center;font-size: 16px;">
         <p>联系我们</p>
         <p>Tel：400-111-0611</p>
@@ -285,99 +144,6 @@
   </div>
 
   <script type="text/javascript">
-
-    function setDemo_begin(index){
-      var div = '<div>'
-          + '<div id="demo' + index
-          + '" class="carousel slide" data-ride="carousel" style="text-align: center;padding-top: 5%;">'
-          + '<ul class="carousel-indicators">'
-          + '<li data-target="#demo' + index
-          + '" data-slide-to="0" class="active"></li>'
-          + '<li data-target="#demo' + index
-          + '" data-slide-to="1"></li>'
-          + '<li data-target="#demo' + index
-          + '" data-slide-to="2"></li>'
-          + '</ul>'
-          + '<div class="carousel-inner">'
-      return div
-    } 
-
-    function setDemo_center(index,item){
-      var div = ""
-      if (index == 0 || index == 3 || index == 6) {
-        div += '<div class="carousel-item active">'
-      } else {
-        div += '<div class="carousel-item">'
-      }
-      div += '<a href="camp.html?id=' + item.camp_id + '">'
-          + '<img class="img-thumbnail lunboimg1" src="'
-          + getPath(item.title_pic) + '">'
-          + '</div>'
-      return div
-    } 
-
-    function setDemo_end(index){
-      var div = '</div>'
-          + '<a class="carousel-control-prev" href="#demo' + index
-          + '" data-slide="prev">'
-          + '<span class="carousel-control-prev-icon"></span>'
-          + '</a>'
-          + '<a class="carousel-control-next" href="#demo' + index
-          + '" data-slide="next">'
-          + '<span class="carousel-control-next-icon"></span>'
-          + '</a>'
-          + '</div>'
-          + '</div>'
-      return div
-    } 
-
-    function getPath(item){
-      var url = "https://www.gfcamps.cn/images/camps/"
-          + item
-      return url;
-    } 
-
-    function initNine(){
-        $.ajax({
-          type:'POST',
-          url:'https://www.gfcamps.cn/getNine',
-          dataType:'json',
-          success:function(data){
-            for (var i = 0; i < 3; i++) {
-              var html = setDemo_begin(i);
-              if (i == 0){
-                for (var j = 0; j < 3; j++) {
-                  html += setDemo_center(j,data[j])
-                }
-              }
-              else if (i == 1){
-                for (var j = 3; j < 6; j++) {
-                  html += setDemo_center(j,data[j])
-                }
-              }
-              else if (i == 2){
-                for (var j = 6; j < 9; j++) {
-                  html += setDemo_center(j,data[j])
-                }
-              }
-              html += setDemo_end(i)
-              var demo = "#demo_" + i
-              console.log(html)
-             $(demo).html(html);
-            }
-          },
-          error:function(hex){
-            //alert("错误返回");
-            console.log("错误返回");
-          }
-        }) 
-    }
-
-    //不分页
-    $(document).ready(function(){
-        initNine()
-    })
-    
   </script>
 </body>
 </html>
